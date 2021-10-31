ddfeeca66c5994b516986fd05778fbf654025b8c
AqulLE <91650898+AqulLE@users.noreply.github.com>
2021-10-31 21:42:50 +0900

    Initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int[] array = {10,20,30,40,50 };
+
+        for (int i = 0; i < array.Length; i++)
+            Debug.Log(array[i]);
+
+        for (int i = array.Length - 1; i >= 0; i--)
+            Debug.Log(array[i]);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
